e4d6db2c6665367c23273fbab9e6dec7173f3a2e
Ulvy <romy.ulvy@passapptech.com.kh>
2026-08-12 22:21:19 +0700

    fix grid

--- app/pages/user/favourites.vue
+++ app/pages/user/favourites.vue
@@ -30,7 +30,7 @@
     </div>
 
     <!-- Property grid -->
-    <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div v-else class="grid md:grid-cols-2 grid-cols-1 lg:grid-cols-4 gap-6">
       <BrowsePropertyCard
         v-for="item in items"
         :key="item.id"
